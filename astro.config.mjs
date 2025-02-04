e24a57716747e63325c246e58f761721d4e7036c
Rob <139673852+robertparnell@users.noreply.github.com>
2025-02-04 14:30:34 -0800

    Resume

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://shanetrimbur.github.io',
+  base: '/',
+  integrations: [tailwind()],
+});
