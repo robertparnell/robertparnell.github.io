15256fdaac09aeaa1942ca65b1d5288123f6554c
robert.parnell <robert.parnell01@gmail.com>
2025-02-05 10:27:19 -0800

    Fixed TypeScript errors and missing exports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import { defineConfig } from "astro/config";
-=======
-﻿import { defineConfig } from "astro/config";
->>>>>>> 1aa80ec (Reinitialized repository in the correct location)
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
-  site: 'https://shanetrimbur.github.io',
-=======
   site: 'https://robertparnell.github.io',
->>>>>>> 1aa80ec (Reinitialized repository in the correct location)
   base: '/',
   integrations: [tailwind()],
 });
