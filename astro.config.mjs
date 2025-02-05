bc8a6c210fbf8359a028debbab50437b9fd4eedf
robert.parnell <robert.parnell01@gmail.com>
2025-02-05 09:28:49 -0800

    Reinitialized repository in the correct location

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
 import { defineConfig } from "astro/config";
+=======
+﻿import { defineConfig } from "astro/config";
+>>>>>>> 1aa80ec (Reinitialized repository in the correct location)
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+<<<<<<< HEAD
   site: 'https://shanetrimbur.github.io',
+=======
+  site: 'https://robertparnell.github.io',
+>>>>>>> 1aa80ec (Reinitialized repository in the correct location)
   base: '/',
   integrations: [tailwind()],
 });
